17db1de8062f5314ac6213784b14907ea80e0181
Vladimir Rodkin <mail@vovanr.com>
2016-04-11 21:55:44 +0300

    test(lib): Add handlers tests

--- test.js
+++ test.js
@@ -14,12 +14,6 @@ test.afterEach(() => {
 	delete global.window;
 });
 
-function createComponent(component, props = {}) {
-	const shallowRenderer = ReactTestUtils.createRenderer();
-	shallowRenderer.render(React.createElement(component, props));
-	return shallowRenderer.getRenderOutput();
-}
-
 test(t => {
 	const result = createComponent(C);
 
@@ -33,17 +27,17 @@ test('render childrens', t => {
 		min: 2,
 		max: 9
 	};
-	const result = ReactTestUtils.renderIntoDocument(React.createElement(C, props));
+	const result = renderIntoDocument(props);
 
-	const dec = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'dec-inc__control_type_dec');
+	const dec = getDec(result);
 	t.is(dec.nodeName, 'SPAN');
 	t.is(dec.className, 'dec-inc__control dec-inc__control_type_dec');
 
-	const value = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'dec-inc__value');
+	const value = getValue(result);
 	t.is(value.nodeName, 'INPUT');
 	t.is(value.className, 'dec-inc__value');
 
-	const inc = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'dec-inc__control_type_inc');
+	const inc = getInc(result);
 	t.is(inc.nodeName, 'SPAN');
 	t.is(inc.className, 'dec-inc__control dec-inc__control_type_inc');
 });
@@ -53,9 +47,9 @@ test('disable dec if value is min', t => {
 		value: 5,
 		min: 5
 	};
-	const result = ReactTestUtils.renderIntoDocument(React.createElement(C, props));
+	const result = renderIntoDocument(props);
 
-	const dec = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'dec-inc__control_type_dec');
+	const dec = getDec(result);
 	t.regex(dec.className, /dec-inc__control_disabled/);
 });
 
@@ -64,9 +58,9 @@ test('disable inc if value is max', t => {
 		value: 5,
 		max: 5
 	};
-	const result = ReactTestUtils.renderIntoDocument(React.createElement(C, props));
+	const result = renderIntoDocument(props);
 
-	const inc = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'dec-inc__control_type_inc');
+	const inc = getInc(result);
 	t.regex(inc.className, /dec-inc__control_disabled/);
 });
 
@@ -78,3 +72,249 @@ test('allow to add custom class name', t => {
 
 	t.is(result.props.className, 'dec-inc foo bar');
 });
+
+test('dec Click', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const dec = getDec(result);
+
+	ReactTestUtils.Simulate.click(dec);
+	t.is(count, 4);
+});
+
+test('inc Click', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const inc = getInc(result);
+
+	ReactTestUtils.Simulate.click(inc);
+	t.is(count, 6);
+});
+
+test('value ArrowDown', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.keyUp(value, {key: 'ArrowDown'});
+	t.is(count, 4);
+});
+
+test('value ArrowUp', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.keyUp(value, {key: 'ArrowUp'});
+	t.is(count, 6);
+});
+
+test('value PageDown', t => {
+	let count = 55;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.keyUp(value, {key: 'PageDown'});
+	t.is(count, 45);
+});
+
+test('value PageUp', t => {
+	let count = 55;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.keyUp(value, {key: 'PageUp'});
+	t.is(count, 65);
+});
+
+test('value End', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		min: -3,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.keyUp(value, {key: 'End'});
+	t.is(count, -3);
+});
+
+test('value Home', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		max: 33,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.keyUp(value, {key: 'Home'});
+	t.is(count, 33);
+});
+
+test('value End without min', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.keyUp(value, {key: 'End'});
+	t.is(count, 5);
+});
+
+test('value Home without max', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.keyUp(value, {key: 'Home'});
+	t.is(count, 5);
+});
+
+test('value Change', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	value.value = '3';
+	ReactTestUtils.Simulate.change(value);
+	t.is(count, 3);
+
+	value.value = 'foo';
+	ReactTestUtils.Simulate.change(value);
+	t.is(count, 3, 'only Number');
+});
+
+test('value Wheel down', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.wheel(value, {deltaY: 5});
+	t.is(count, 5, 'without focus');
+
+	ReactTestUtils.Simulate.focus(value);
+	ReactTestUtils.Simulate.wheel(value, {deltaY: 5});
+	t.is(count, 4);
+
+	ReactTestUtils.Simulate.blur(value);
+	ReactTestUtils.Simulate.wheel(value, {deltaY: 5});
+	t.is(count, 4, 'without focus');
+});
+
+test('value Wheel up', t => {
+	let count = 5;
+	const props = {
+		value: count,
+		onChange(value) {
+			count = value;
+		}
+	};
+	const result = renderIntoDocument(props);
+	const value = getValue(result);
+
+	ReactTestUtils.Simulate.wheel(value, {deltaY: -5});
+	t.is(count, 5, 'without focus');
+
+	ReactTestUtils.Simulate.focus(value);
+	ReactTestUtils.Simulate.wheel(value, {deltaY: -5});
+	t.is(count, 6);
+
+	ReactTestUtils.Simulate.blur(value);
+	ReactTestUtils.Simulate.wheel(value, {deltaY: -5});
+	t.is(count, 6, 'without focus');
+});
+
+// Shallow renderer
+function createComponent(component, props = {}) {
+	const shallowRenderer = ReactTestUtils.createRenderer();
+	shallowRenderer.render(React.createElement(component, props));
+	return shallowRenderer.getRenderOutput();
+}
+
+// Document renderer
+function renderIntoDocument(props) {
+	return ReactTestUtils.renderIntoDocument(React.createElement(C, props));
+}
+
+function getDec(component) {
+	return getElem(component, 'dec-inc__control_type_dec');
+}
+
+function getInc(component) {
+	return getElem(component, 'dec-inc__control_type_inc');
+}
+
+function getValue(component) {
+	return getElem(component, 'dec-inc__value');
+}
+
+function getElem(component, className) {
+	return ReactTestUtils.findRenderedDOMComponentWithClass(component, className);
+}
