3036fe988b85135e8b757786966a01247f5da734
Vladimir Rodkin <mail@vovanr.com>
2016-04-24 20:28:55 +0300

    gh-pages update

---
+++ app.jsx
@@ -0,0 +1,74 @@
+/* global document, React, ReactDOM */
+
+import DecInc from '../index.jsx';
+
+require('../style.styl');
+require('./style.styl');
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			count: 33,
+			gramm: 0.15
+		};
+		this.handleChangeCount = this.handleChangeCount.bind(this);
+		this.handleChangeGramm = this.handleChangeGramm.bind(this);
+	}
+
+	handleChangeCount(value) {
+		this.setState({count: value});
+	}
+
+	handleChangeGramm(value) {
+		this.setState({gramm: value});
+	}
+
+	render() {
+		return (
+			<div
+				style={{
+					fontSize: '500%',
+					fontFamily: 'monospace'
+				}}
+				>
+				<div>
+					<label>
+						<small>{'Count: '}</small>
+						<DecInc
+							className="dec-inc_theme_example"
+							value={this.state.count}
+							max={33}
+							min={0}
+							onChange={this.handleChangeCount}
+							/>
+					</label>
+				</div>
+				<div>
+					<label>
+						<small>{'Gramm: '}</small>
+						<DecInc
+							className="dec-inc_theme_example"
+							value={this.state.gramm}
+							min={0}
+							step={0.001}
+							onChange={this.handleChangeGramm}
+							/>
+					</label>
+				</div>
+				<div>
+					<label>
+						<small>{'Round: '}</small>
+						<DecInc
+							className="dec-inc_theme_example"
+							value={(Math.round(this.state.gramm * this.state.count))}
+							disabled
+							/>
+					</label>
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
